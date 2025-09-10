02980ad1f96167a5a9f0244646255c5b80b7673b
Raunak <160758549+Raunak216@users.noreply.github.com>
2025-09-10 10:19:41 +0530

    Update environment.js backend url

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let isProd = true;
 const server = isProd
-  ? "https://mindspace-webrtc-videocall-1.onrender.com"
+  ? "https://mindspace-webrtc-videocall-2.onrender.com"
   : "http://localhost:8080";
 
 export default server;
