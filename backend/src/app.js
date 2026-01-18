d04ea6db2b69ddf332a256185d0f57c6c7dba758
Raunak <raunakzyx2@gmail.com>
2026-01-18 23:24:34 +0530

    login fix2

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,21 +17,7 @@ const allowedOrigins = [
   "https://mindspace-webrtc-videocall-3.onrender.com",
 ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }),
-);
+app.use(cors());
 
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
