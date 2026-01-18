e70ebf189656724c8aa840a9777482694c0a72eb
Raunak <raunakzyx2@gmail.com>
2026-01-18 22:58:45 +0530

    login issue fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,27 @@ const io = connectToSocket(server);
 
 app.set("port", process.env.PORT || 8080);
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://mindspace-webrtc-videocall-static.onrender.com",
+  "https://mindspace-webrtc-videocall-3.onrender.com",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
